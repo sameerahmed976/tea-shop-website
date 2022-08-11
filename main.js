38c4d80f1d3b15744dc311dad5cf961f04c1bfbe
mohammed sameer ahmed <sameerahmed976@gmail.com>
2022-08-11 15:57:08 +0530

    added menu

---
+++ main.js
@@ -0,0 +1,27 @@
+const hamburgerBtn = document.querySelector(".hamburger");
+const sidebar = document.querySelector(".sidebar");
+const sidebarLinks = document.querySelectorAll(".sidebar li a");
+// const navLinksActive = document.querySelector(".nav-links-active");
+
+hamburgerBtn.addEventListener("click", () => {
+  //   console.log("click");
+
+  if (!hamburgerBtn.classList.contains("hamburger-close")) {
+    hamburgerBtn.classList.add("hamburger-close");
+    sidebar.classList.add("sidebar-active");
+    document.body.style.backgroundColor = "rgba(0,0,0,0.2)";
+  } else {
+    hamburgerBtn.classList.remove("hamburger-close");
+    sidebar.classList.remove("sidebar-active");
+    document.body.style.backgroundColor = "";
+  }
+});
+
+// remove sidebar
+sidebarLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburgerBtn.classList.remove("hamburger-close");
+    sidebar.classList.remove("sidebar-active");
+    document.body.style.backgroundColor = "";
+  });
+});
